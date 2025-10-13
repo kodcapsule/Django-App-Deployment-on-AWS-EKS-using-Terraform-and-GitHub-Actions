9f1d5ae2019789e664ea641058a62da630c2c771
Kuseh <kusehsw@gmail.com>
2025-10-13 12:37:49 +0000

    configured backend and AWS OIDC provider

---
+++ Infrastructure/backend/dynamodb-lock.tf
@@ -0,0 +1,22 @@
+resource "aws_dynamodb_table" "tf_state_lock" {
+  name = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+    attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+    tags = {
+        Name        = "Terraform Lock Table"
+        Environment = "Production"
+    }
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true 
+  }
+}
