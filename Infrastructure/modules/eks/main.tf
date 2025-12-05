0c4cf65983af25a6643d9838f47f84e6deb87f90
Kuseh <kusehsw@gmail.com>
2025-12-05 09:43:20 +0000

    test1019

--- Infrastructure/modules/eks/main.tf
+++ Infrastructure/modules/eks/main.tf
@@ -1,13 +1,13 @@
 
 
-# resource "aws_s3_bucket" "test_bucket" {
-#   bucket = "my-unique-bucket-name-12345"
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-unique-bucket-name-12345"
   
-# }
+}
 
 
-# resource "aws_s3_bucket_acl" "test101" {
-#     bucket = aws_s3_bucket.test_bucket.id
-#     acl    = "private"
+resource "aws_s3_bucket_acl" "test101" {
+    bucket = aws_s3_bucket.test_bucket.id
+    acl    = "private"
   
-# }
+}
