dacdd3748afe5893c0e61ae026ecfd3be22871b8
KUSEH SIMON WEWOLIAMO <kusehsw@gmail.com>
2026-01-08 00:48:43 +0000

    pipline completed (#3)
    * Add unit testing stage to GitHub Actions workflow and uncomment S3 bucket resources in Terraform
    
    * Comment out S3 bucket and ACL resource definitions in Terraform; update GitHub Actions workflow to disable terraform-compliance tests
    
    * Update working directory for Terratest in GitHub Actions; uncomment S3 bucket resources in Terraform
    
    * Comment out Terratest dependencies and unit test steps; update Terraform native test execution in workflow
    
    * Uncomment Terratest installation and testing steps; restore S3 bucket and ACL resource definitions in Terraform
    
    * Rename unit testing job to include integration tests; remove Terratest setup and dependencies; add VPC CIDR block unit test
    
    * Refactor VPC CIDR block variable naming and remove obsolete unit test
    
    * Uncomment VPC module configuration in Terraform
    
    * updated
    
    * Add Terraform configuration for EC2 instance with user data script
    
    * Add AWS region environment variable and update EC2 instance user data message
    
    * Refactor CI/CD pipeline and add manual approval step; update EC2 instance user data message
    
    * Update EC2 instance user data message to include instance identifier
    
    * Refactor Terraform apply stage in CI/CD pipeline; add necessary steps and dependencies

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,11 +1,28 @@
-# use the vpc module to create a VPC
-module "vpc" {
-  source = "./modules/vpc"
-  VPC-CIDR-BLOCK = var.VPC-CIDR-BLOCK
-  VPC-NAME       = var.VPC-NAME
-  REGION         = var.REGION
-  PUBLIC-SUBNETS  = var.PUBLIC-SUBNETS
-  PRIVATE-SUBNETS = var.PRIVATE-SUBNETS
+resource "aws_instance" "web_server" {
+  ami                    = "ami-0ecb62995f68bb549"
+  instance_type          = "t3.micro"
+  
 
-  TAGS = var.TAGS
+  # User data script to install and start a web server
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello from 98 EC2 Instance!!</h1>" > /var/www/html/index.html
+              EOF
+
+  # Root volume configuration
+  root_block_device {
+    volume_size           = 8
+    volume_type           = "gp3"
+    delete_on_termination = true
+    encrypted             = true
+  }
+
+  tags = {    
+    Environment = "Development"
+    ManagedBy   = "Terraform"
+  }
 }
