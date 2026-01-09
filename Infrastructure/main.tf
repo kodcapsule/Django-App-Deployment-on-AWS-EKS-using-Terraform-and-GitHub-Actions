e8151401daf8db41c36622f2e9bd400cf4e2e629
Kuseh <kusehsw@gmail.com>
2026-01-09 15:03:46 +0000

    Add Terraform plan generation step and fix typo in EC2 instance user data message

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "web_server" {
               yum install -y httpd
               systemctl start httpd
               systemctl enable httpd
-              echo "<h1>Hello from   EC2 Instance!!</h1>" > /var/www/html/index.html
+              echo "<h1>Hello from   EC2 Instance!I!</h1>" > /var/www/html/index.html
               EOF
 
   # Root volume configuration
